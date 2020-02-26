b251cb66b1e1a55f5afe3942f046be77cae2f025
TayyebChaudhary <chtayyeb250@gmail.com>
2020-02-26 18:17:04 +0500

    testing commit

---
+++ src/Container/Mainpage/mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section1 from '../../Components/Sections/Section1/section1';
+import Sectio2 from '../../Components/Sections/Section2/section2'
+import Section2 from '../../Components/Sections/Section2/section2';
+export default function mainpage() {
+    return (
+        <div>
+        <Section1/>
+        <Section2/>
+        </div>
+    )
+}
